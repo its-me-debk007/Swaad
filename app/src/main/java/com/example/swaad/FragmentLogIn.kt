f59f4a4c098bc6cd46bf8fc977291abba7934168
its-me-debk007 <debashish.joy@gmail.com>
2021-10-31 11:31:35 +0530

    Updated layout for login and Forgot Password 1

---
+++ app/src/main/java/com/example/swaad/FragmentLogIn.kt
@@ -0,0 +1,6 @@
+package com.example.swaad
+
+import androidx.fragment.app.Fragment
+
+class FragmentLogIn: Fragment(R.layout.fragment_reference_sign_up) {
+}
--- app/src/main/java/com/example/swaad/MainActivity.kt
+++ app/src/main/java/com/example/swaad/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.swaad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.example.swaad.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+//    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+//        setContentView(binding.root)
+
+        val signUp = findViewById<TextView>(R.id.loginSignUpText)
+//        signUp.setOnClickListener(View.OnClickListener {
+//            val intent = Intent(this, ::class.java)
+//            startActivity(intent)
+//        })
+
+
     }
 }
