4d4b8c2d15479786063b64fadd102baca89a546c
Kunal Mehrotra <kunalmehrotra2001@gmail.com>
2021-11-07 23:52:50 +0530

    added grid layout in recycler view layout and added coloumn spacing bw grid

--- app/src/main/java/com/example/swaad/home_page.kt
+++ app/src/main/java/com/example/swaad/home_page.kt
@@ -1,8 +1,10 @@
 package com.example.swaad
 
 import android.content.Context
+import android.content.Context.LOCATION_SERVICE
 import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import android.location.LocationRequest
 import android.os.Bundle
@@ -15,35 +17,66 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.location.LocationManagerCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import java.lang.Exception
 import java.util.jar.Manifest
+import android.R
+import android.R.attr.spacing
+
+import GridSpacingItemDecoration
+import android.R.attr
+
 
 class home_page : Fragment() {
 
-//
+    //
 //    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 //    lateinit var locationRequest: LocationRequest
 ////  the permission id is just an int that must be unique so you can use any number
 //    val permission_id=100
     private var layoutManager:RecyclerView.LayoutManager?=null
-    private var adapter:RecyclerView.Adapter<RecyclerAdapter.ViewHolder>?=null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
+    //    private lateinit var locationManager:LocationManager
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val v = inflater.inflate(R.layout.fragment_home_page, container, false)
-        layoutManager=LinearLayoutManager(container?.context)
-        val recyclerView = v.findViewById<RecyclerView>(R.id.RecyclerView)
-        recyclerView.layoutManager=layoutManager
-        adapter=RecyclerAdapter()
-        recyclerView.adapter=adapter
+        val v= inflater.inflate(com.example.swaad.R.layout.fragment_home_page, container, false)
 
+        //Runtimepermissions
+//        if (container != null) {
+//            if (ContextCompat.checkSelfPermission(
+//                    container.context,
+//                    android.Manifest.permission.ACCESS_FINE_LOCATION
+//                ) != PackageManager.PERMISSION_GRANTED
+//            ) {
+//                ActivityCompat.requestPermissions(MainActivity(), new String []
+//                {
+//                    android.Manifest.permission.ACCESS_FINE_LOCATION
+//                }, 100
+//                );
+//            }
+//            val location_text = v.findViewById<TextView>(R.id.LocationText)
+//            location_text.text = getlocation()
+//        }
+        layoutManager = GridLayoutManager(container?.context, 2)
+        val recyclerView = v.findViewById<RecyclerView>(com.example.swaad.R.id.RecyclerView)
+        var spanCount=2
+        var spacing = 30
+        var includeEdge=false
+        recyclerView.addItemDecoration(GridSpacingItemDecoration(spanCount, spacing, includeEdge))
+        recyclerView.layoutManager = layoutManager
+        adapter = RecyclerAdapter()
+        recyclerView.adapter = adapter
 
 //        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 //        //fun that will allow to get last location
@@ -73,7 +106,28 @@ class home_page : Fragment() {
 //        }
 
         return v
-    }}
+    }
+}
+//LocationManager class provides the facility to get latitude and longitude
+//    private fun getlocation(): CharSequence? {
+//        try {
+//            locationManager = (LocationManager) getApplicationContext ().getSystemService(LOCATION_SERVICE)
+//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,5)
+//        }
+//        catch ( e:Exception)
+//        {
+//            e.printStackTrace()
+//        }
+//
+//
+//    }
+//
+//    override fun onLocationChanged(location: Location) {
+//
+//
+//    }
+
+//}
     //        var fusedLocationProviderClient : FusedLocationProviderClient
 //        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient()
 
