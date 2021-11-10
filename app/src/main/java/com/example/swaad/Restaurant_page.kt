f46fa671bef4531da5a8ab1832142850cb8df337
Kunal Mehrotra <kunalmehrotra2001@gmail.com>
2021-11-10 11:45:36 +0530

    added few more images on restaurant page

--- app/src/main/java/com/example/swaad/Restaurant_page.kt
+++ app/src/main/java/com/example/swaad/Restaurant_page.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,6 +23,14 @@ class Restaurant_page : Fragment() {
         // Inflate the layout for this fragment
         val v= inflater.inflate(R.layout.fragment_restaurant_page, container, false)
         layoutManager = LinearLayoutManager(container?.context)
+        val back = v.findViewById<ImageView>(R.id.back)
+        back.setOnClickListener {
+            val fragmentManager = activity?.supportFragmentManager
+            val fragmentTransaction = fragmentManager?.beginTransaction()
+            fragmentTransaction?.replace(R.id.fragment_container, home_page())
+            fragmentTransaction?.addToBackStack(null)
+            fragmentTransaction?.commit()
+        }
         val recyclerViewHomePage = v.findViewById<RecyclerView>(R.id.recyclerViewRestaurantPage)
         recyclerViewHomePage.layoutManager = layoutManager
         adapter = RecyclerAdapterRestaurantPage()
